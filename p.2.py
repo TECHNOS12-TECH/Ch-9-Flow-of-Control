25828da459ad1bc35d4d8e111e0e5f0596b28806
BINARY BEASTS <83653797+TECHNOS12-TECH@users.noreply.github.com>
2021-05-22 22:16:36 +0530

    Add files via upload

---
+++ p.2.py
@@ -0,0 +1,9 @@
+a=int(input("Enter your 1st number:"))
+b=int(input("Enter your 2nd number:"))
+c=int(input("Enter your 3rd number:"))
+max=a
+if b>max:
+    max=b
+if c>max:
+    max=c
+print("The greatest of three number is:",max)
