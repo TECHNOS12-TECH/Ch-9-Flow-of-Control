f7838ff8b063ab089e323db2eedbafa511899ac9
BINARY BEASTS <83653797+TECHNOS12-TECH@users.noreply.github.com>
2021-05-24 15:16:45 +0530

    Add files via upload

---
+++ type c1.py
@@ -0,0 +1,5 @@
+length=float(input("Enter the length you want to convert:\n"))
+if length<0:
+    print("You have entered invalid entry (Please enter positive value)")
+else:
+    print("Your length is inches is:",length/2.54) 
