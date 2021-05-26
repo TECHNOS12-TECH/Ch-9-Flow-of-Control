f8de29c36ccf00656b74c529ef517c67cb02442e
BINARY BEASTS <83653797+TECHNOS12-TECH@users.noreply.github.com>
2021-05-26 23:03:10 +0530

    Add files via upload

---
+++ p.17.py
@@ -0,0 +1,13 @@
+print("Example of statement of break")
+for i in range(1,11):
+    if i%7==0:
+        break
+    else:
+        print(i)
+print()
+print("Example of statement continue")
+for i in range(1,11):
+    if i%7==0:
+        continue
+    else:
+        print(i)
