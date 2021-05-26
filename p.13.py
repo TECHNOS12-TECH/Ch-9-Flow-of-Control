67d3859e5399f808111aa0678c97f7e0f3f9d370
BINARY BEASTS <83653797+TECHNOS12-TECH@users.noreply.github.com>
2021-05-26 22:08:41 +0530

    Add files via upload

---
+++ p.13.py
@@ -0,0 +1,7 @@
+total=0
+for i in range(1,8):
+    total+=i
+print("Sum of natural numbers between 1 to 7 is:",total)
+print()
+print("Other way to write")
+print("Sum of natural numbers <=7 is:",total)
