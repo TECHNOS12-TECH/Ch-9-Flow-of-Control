32fc5eb3b76a663a5e3cf4ee5aed58633f17f5da
BINARY BEASTS <83653797+TECHNOS12-TECH@users.noreply.github.com>
2021-05-27 16:09:20 +0530

    Add files via upload

---
+++ NCERT1.py
@@ -0,0 +1,6 @@
+n=input("Enter your name: ")
+a=int(input("Enter your age: "))
+if a>=18:
+    print(n,"You are eligible to apply for driving license")
+else:
+    print(n,"You are not eligible to apply for driving license")
